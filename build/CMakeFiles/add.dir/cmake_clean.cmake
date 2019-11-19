150b3d2085970157bc14025258935a49374e0404
TommyD <tommyd@TommyDdeMac-mini.local>
2019-11-19 18:20:46 +0800

    init

---
+++ build/CMakeFiles/add.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/add.dir/add.c.o"
+  "libadd.pdb"
+  "libadd.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import ConanFile, CMake
+from distutils.dir_util import copy_tree
+
+class AddConan(ConanFile):
+    name = "add"
+    version = "0.1"
+    settings = "os","arch","build_type","compiler"
+    
+    #options = {
+    #    "macos_minversion": ["10.10"],
+    #    "macos_sdk": ["10.14"]
+    #}    
+
+    def source(self):
+        print("sourcing...")
+        copy_tree("/Users/tommyd/Projects/test/add", ".")
+    
+    def build(self):
+        cmake = CMake(self) #self._configure_cmake()#CMake(self)
+        cmake.configure(source_dir="src")
+        cmake.build()
+        #cmake.install()
+   
+    def package(self):
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+ 
+    def package_info(self):
+        self.cpp_info.libs = ["add"]
